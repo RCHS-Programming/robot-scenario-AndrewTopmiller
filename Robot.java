e4aa86fcb5d4bb0a55aeefb98285808e4d87c238
AndrewTopmiller <rjtopmiller@outlook.com>
2021-04-01 08:41:36 -0400

    added sounds

--- Robot.java
+++ Robot.java
@@ -8,14 +8,29 @@ import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
  */
 public class Robot extends Actor
 {
+    private GreenfootImage robotimage1;
+    private GreenfootImage robotimage2; 
+    private int lives;
+    private int score;
+    private int pizzaEaten;
     /**
      * Act - do whatever the Robot wants to do. This method is called whenever
      * the 'Act' or 'Run' button gets pressed in the environment.
      */
+    public Robot()
+    {
+         robotimage1=new GreenfootImage("man01.png");
+         robotimage2=new GreenfootImage("man02.png");
+         lives=3;
+         score=0;
+         pizzaEaten=0;
+    }
     public void act() 
     {
        robotMovement();
-       detectWallCollision();// Add your action code here.
+       detectWallCollision();
+       detectHome();
+       eatPizza();// Add your action code here.
     }   
     
     public void robotMovement()
@@ -36,6 +51,14 @@ public class Robot extends Actor
         {
             setLocation( getX()-3, getY() );
         }
+        if(getImage()== robotimage1)
+        {
+            setImage( robotimage2);
+        }
+        else
+        {
+            setImage ( robotimage1);
+        } 
     }
     
     public void detectWallCollision()
@@ -43,6 +66,33 @@ public class Robot extends Actor
         if (isTouching(Wall.class))
         {
             setLocation(30, 40);
+            Greenfoot.playSound("hurt.wav");
         }
     }
+    public void detectHome()
+    {
+        if (pizzaEaten == 5 )
+        {
+            Greenfoot.stop();
+            setLocation(30, 40);
+            Greenfoot.playSound("yipee.wav");
+        }
+        if (isTouching(Home.class))
+        {
+            setLocation(30, 40);
+            Greenfoot.playSound("yipee.wav");
+            
+        }
+    }
+    public void eatPizza()
+    {
+        if (isTouching(Pizza.class))
+        {
+            removeTouching(Pizza.class);
+            Greenfoot.playSound("eat.wav");
+            pizzaEaten = pizzaEaten + 1;
+            
+        }
+    }
+    
 }
